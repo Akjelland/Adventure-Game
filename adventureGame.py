0e715fdf045bc4fe886c414546e8e41bd045c286
Akjelland <kjeandb@dunwoody.edu>
2026-02-11 11:45:00 -0600

    Update adventureGame.py

--- adventureGame.py
+++ adventureGame.py
@@ -28,16 +28,132 @@ story = {
         "next": {}
     },
     "raft_success": {
-        "question": "You built a raft and crossed safely. You win!",
+        "question": "You built a raft and crossed safely. what now?",
+        "choices": {"a":"talk to stranger","b":"go to mountain"},
+        "next": {"a":"stranger","b":"mountain"}
+    },
+        "stranger": {
+        "question": "You walk up to the stranger, What will you do?",
+        "choices": {"a":"awkwardly leave","b":"ask for information"},
+        "next": {"a":"raft_success","b":"information"}
+    },
+        "information": {
+        "question": "the stranger tells you about a dragon in a castle near here. What now?",
+        "choices": {"a":"thank stranger","b":"leave"},
+        "next": {"a":"right_path","b":"leave_mean"}
+    },
+        "thank stranger": {
+        "question": "You thank the stranger and then peacefully leave. what now?",
+        "choices": {"a":"go to mountain","b":"go to village"},
+        "next": {"a":"mountain","b":"village"}
+    },
+        "village": {
+        "question": "You go to the village and live a peaceful life. You win!",
+        "choices": {},
+        "next": {}
+    },
+        "leave_mean": {
+        "question": "The stranger is not pleased by your lack of manners and kills you. You lose!",
+        "choices": {},
+        "next": {}
+    },
+        "mountain": {
+        "question": "you go towards the mountain and come accross a castle surounded by lava. what now?",
+        "choices": {"a":"turn around","b":"enter castle"},
+        "next": {"a":"raft_success","b":"castle"}
+    },
+        "castle": {
+        "question": "You enter the castle it seems very warn down. what now?",
+        "choices": {"a":"grand room adorned in gold","b":"dark hallway"},
+        "next": {"a":"grand_room","b":"dark_hallway"}
+    },
+        "dark_hallway": {
+        "question": "You enter the hallway and see a couple rooms. What now?",
+        "choices": {"a":"Room 1","b":"Room 2"},
+        "next": {"a":"room_1","b":"room_2"}
+    },
+        "room_1": {
+        "question": "You find the treasury! You make your great escape and live a life of luxury. You win!",
+        "choices": {},
+        "next": {}
+    },
+        "room_2": {
+        "question": "You enter the room and notice a large hole in the wall that appears to go to a large room. What now?",
+        "choices": {"a":"approach hole","b":"leave room"},
+        "next": {"a":"ledge_scene","b":"dark_hallway"}
+    },
+        "ledge_scene": {
+        "question": "You see a dragon and a large chandelier attached by a chain. What now?",
+        "choices": {"a":"attempt to break chain","b":"leave room"},
+        "next": {"a":"chain_scene","b":"dark_hallway"}
+    },
+        "chain_scene": {
+        "question": "You sucessfully break the chain and it falls and hits the dragon killing it! You win!",
+        "choices": {},
+        "next": {}
+    },
+        "grand_room": {
+        "question": "Its a dragon! What now?",
+        "choices": {"a":"turn back","b":"bravely fight"},
+        "next": {"a":"castle","b":"bravely_fight"}
+    },
+        "bravely_fight": {
+        "question": "You go to confront the dragon but notice you are extremely underprepared and you die bravely fighting. You lose but bravely!",
         "choices": {},
         "next": {}
     },
     "torch_success": {
-        "question": "The torch lights up the cave. You find treasure. You win!",
+        "question": "You see the cave branches in two directions. What do you do?",
+        "choices": {"a":"right path","b":"left path"},
+        "next": {"a":"right_path","b":"left_path"}
+    },
+    "right_path": {
+        "question": "you go right and see 2 chests, Which will you choose?",
+        "choices": {"a":"gold chest","b":"wooden chest"},
+        "next": {"a":"trap","b":"treasure"}
+    },
+        "left_path": {
+        "question": "you go left and see what looks like a goblin, what will you do?",
+        "choices": {"a":"fight","b":"run"},
+        "next": {"a":"fight_goblin","b":"start"}
+    },
+        "trap": {
+        "question": "It was a trap. Game over!",
         "choices": {},
         "next": {}
-    }
+    },
+        "treasure": {
+        "question": "You find treasure! Enjoy your life of luxury. You win!",
+        "choices": {},
+        "next": {}
+    },
+        "fight_goblin": {
+        "question": "You approach the goblin. what now?",
+        "choices": {"a":"punch goblin","b":"look for a weapon"},
+        "next": {"a":"punch_goblin","b":"weapon_search"}
+    },
+        "punch_goblin": {
+        "question": "You punch the goblin and die for the effort. you lose!",
+        "choices": {},
+        "next": {}
+    },
+        "weapon_search": {
+        "question": "you look around the cave for some form of weapon, what do you choose?",
+        "choices": {"a":"rusty sword","b":"rock"},
+        "next": {"a":"rusty_sword","b":"rock_path"}
+    },
+        "rusty_sword": {
+        "question": "You choose the rusty sword and it breaks. You lose!",
+        "choices": {},
+        "next": {}
+    },
+        "rock_path": {
+        "question": "You choose the rock and successfully scare off the goblin. You win!",
+        "choices": {},
+        "next": {}
+    },
 }
+
 
 
 question_label = tk.Label(root, text="", wraplength=380)
